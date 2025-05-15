590c4eeca97cf93e616ec12bbe9383186b463dc6
hamza rana <hamza.rana@mail.utoronto.ca>
2025-05-15 13:20:31 -0400

    Multiples creation

---
+++ src/Multiples.java
@@ -0,0 +1,14 @@
+public class Multiples {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i < 1000; i++) {
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            if (divisibleBy3 || divisibleBy5) {
+                sum++;
+            }
+        }
+        System.out.println(sum);
+        }
+}
